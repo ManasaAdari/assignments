5a044cfe0f6aa3d3f2e45d52094ee3f4a0ce8e49
Manasa <116288583+ManasaAdari@users.noreply.github.com>
2024-03-20 15:09:43 +0530

    Create 1hard.js

---
+++ 1hard.js
@@ -0,0 +1,6 @@
+c=prompt('enter celsius temperature:')
+const ctf=(c)=>{console.log("celsius to fahrenheit is",((9/5)*c)+32)};
+ctf(c);
+f=prompt('enter fahrenheit temperature:')
+const ftc=(f)=>{console.log("fahrenheit to celsius is",((5/9)*f-32))};
+ftc(f);
